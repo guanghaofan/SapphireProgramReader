5007ee4a0f42bda7e313c484db09350da1a9f8d0
ghfan <guanghao.fan@amd.om>
2015-09-01 17:06:18 +0800

    setup some the basic package for UI controls and xml fiels reader

---
+++ SapphireProgramReader/src/sapphireprogramreader/xmlreader/blockreader/generic.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sapphireprogramreader.xmlreader.blockreader;
+
+/**
+ * 
+ * @author ghfan
+ */
+public class generic {
+    
+}
